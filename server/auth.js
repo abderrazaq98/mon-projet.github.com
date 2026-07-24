abd9c49196d6ca15ae9812f49763e63fc5bfd604
Géomaticien SIG <154512484+abderrazaq98@users.noreply.github.com>
2026-07-24 20:51:10 +0100

    Delete server directory

--- server/auth.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.isAdmin) {
-    return next();
-  }
-  return res.status(401).json({ error: 'Non autorisé. Veuillez vous connecter.' });
-}
-
-module.exports = { requireAuth };
